2db0b5dfc5188a8c9128f835b5d8f4841bfa59e6
Jia Hong <kuanjiahong@gmail.com>
2022-10-12 13:36:17 +0800

    Added personal timetable

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,5 +3,6 @@ from schedule import views
 
 urlpatterns = [
     path("hello", views.hello),
-    path("logs", views.view_logs, name="logs")
+    path("logs", views.view_logs, name="logs"),
+    path("timetable", views.view_personal_timetable, name="timetable")
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,4 @@
+from email.headerregistry import ContentDispositionHeader
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -10,3 +11,12 @@ def view_logs(request):
         "sample_log_list": ["You logged in at 1:00pm", "You added a new course", "You logged out at 3:00pm"],
     }
     return render(request, "schedule/logs.html", context)
+
+def view_personal_timetable(request):
+    context = {
+        "time_period": [
+            "9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", 
+            "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM"
+        ],
+    }
+    return render(request, "schedule/personal_timetable.html", context)
