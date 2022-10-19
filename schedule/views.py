b0931f2e3ad3dd407d5993224f5d9395a91ce002
Jia Hong <kuanjiahong@gmail.com>
2022-10-19 16:49:41 +0800

    Added extra time duration

--- schedule/views.py
+++ schedule/views.py
@@ -8,20 +8,22 @@ def hello(request):
 
 def view_logs(request):
     context = {
-        "sample_log_list": ["You logged in at 1:00pm", "You added a new course", "You logged out at 3:00pm"],
+        "sample_log_list": ["None", "None", "None"],
     }
     return render(request, "schedule/logs.html", context)
 
 def home_page(request):
     time_duration = [
-        "9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", 
-        "2:00PM", "3:00PM", "4:00PM", "5:00PM", 
-        "6:00PM", "7:00PM"
+        "9:00AM", "9:30AM","10:00AM", "10:30AM","11:00AM", "11:30AM","12:00PM", "12:30PM","1:00PM", "1:30PM",
+        "2:00PM", "2:30PM","3:00PM", "3:30PM","4:00PM", "4:30PM","5:00PM", "5:30PM", 
+        "6:00PM", "6:30PM","7:00PM", "7:30PM"
     ]
-    
+
     context = {
         "user_name": "", 
         "last_login": "", 
         "login_time": "", 
-        "time_period": time_duration}
+        "time_period": time_duration
+        }
+    
     return render(request, "schedule/home.html", context)
