c879340fcbf651a69e20325ac8f5d361b3791ac9
Jia Hong <kuanjiahong@gmail.com>
2022-10-19 15:56:07 +0800

    Added home page

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from schedule import views
 
+app_name = 'schedule'
 urlpatterns = [
     path("hello", views.hello),
+    path("home", views.home_page, name="home"),
     path("logs", views.view_logs, name="logs"),
-    path("timetable", views.view_personal_timetable, name="timetable")
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -12,11 +12,16 @@ def view_logs(request):
     }
     return render(request, "schedule/logs.html", context)
 
-def view_personal_timetable(request):
+def home_page(request):
+    time_duration = [
+        "9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", 
+        "2:00PM", "3:00PM", "4:00PM", "5:00PM", 
+        "6:00PM", "7:00PM"
+    ]
+    
     context = {
-        "time_period": [
-            "9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", 
-            "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM"
-        ],
-    }
-    return render(request, "schedule/personal_timetable.html", context)
+        "user_name": "", 
+        "last_login": "", 
+        "login_time": "", 
+        "time_period": time_duration}
+    return render(request, "schedule/home.html", context)
