114f6e6578a926761c5fbfdcf0426c05c065492f
Harry Pham <huanpham@protonmail.com>
2022-11-17 03:08:31 +0800

    make face_capture accept arguments

--- face_recognition/face_capture.py
+++ face_recognition/face_capture.py
@@ -1,13 +1,34 @@
 import cv2
 import os
+import sys, getopt
+
+if len(sys.argv[1:]) == 0:
+    print('Usage: face_capture.py -u <user_name> -n <num_imgs>')
+    sys.exit(2)
+
+# parse input arguments
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hu:n:", ["help", "user_name=", "num_imgs="])
+except getopt.GetoptError as error:
+    print('Usage: face_capture.py -u <user_name> -n <num_imgs>')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        print('face_capture.py -u <user_name> -n <num_imgs>')
+        sys.exit()
+    elif opt in ("-u", "--user_name"):
+        user_name = arg
+    elif opt in ("-n", "--num_imgs"):
+        num_imgs = int(arg)
 
 faceCascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
 
 video_capture = cv2.VideoCapture(0)
 
-# Specify the `user_name` and `NUM_IMGS` here.
-user_name = "EMAIL_ADDRESS"
-NUM_IMGS = 400
+# Specify the `user_name` and `num_imgs` here.
+# user_name = "EMAIL_ADDRESS"
+# num_imgs = 400
 if not os.path.exists('data/{}'.format(user_name)):
     os.mkdir('data/{}'.format(user_name))
 
@@ -18,7 +39,7 @@ fontColor = (102, 102, 225)
 lineType = 2
 
 # Open camera
-for cnt in range(1, NUM_IMGS + 1):
+for cnt in range(1, num_imgs + 1):
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     frame_copy = frame.copy()
@@ -36,7 +57,7 @@ for cnt in range(1, NUM_IMGS + 1):
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    msg = "Saving {}'s Face Data [{}/{}]".format(user_name, cnt, NUM_IMGS)
+    msg = "Saving {}'s Face Data [{}/{}]".format(user_name, cnt, num_imgs)
     cv2.putText(frame, msg,
                 bottomLeftCornerOfText,
                 font,
